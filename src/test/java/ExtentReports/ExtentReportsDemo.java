9891cfe5d71c1d930383daf6b8b29a3bf583c3ad
sudeep-sn <138565877+sudeep-sn@users.noreply.github.com>
2025-11-11 21:07:16 +0530

    extent report to take values from jenkins job

--- src/test/java/ExtentReports/ExtentReportsDemo.java
+++ src/test/java/ExtentReports/ExtentReportsDemo.java
@@ -8,14 +8,20 @@ import com.aventstack.extentreports.reporter.configuration.ResourceCDN;
 public class ExtentReportsDemo {
 	public static ExtentReports getReportObject()
 	{
-		String reportPath = System.getProperty("user.dir")+"//reports//"+"index.html";
+		String reportDir = System.getProperty("reportDir", "target");
+		String reportPath = System.getProperty("user.dir")+ "/" + reportDir + "/index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
 		reporter.config().setResourceCDN(ResourceCDN.EXTENTREPORTS);
 		reporter.config().setDocumentTitle("Test Results");
 		reporter.config().setReportName("Ecommerce Automation");
+		reporter.config().setTheme(Theme.STANDARD);
+		reporter.config().setEncoding("utf-8");
+		reporter.config().setTimeStampFormat("MMM dd, yyyy HH:mm:ss");
 		ExtentReports extent = new ExtentReports();
 		extent.attachReporter(reporter);
 		extent.setSystemInfo("Tester", "Sudeep");
+		extent.setSystemInfo("Build Number", System.getenv("BUILD_NUMBER"));
+		extent.setSystemInfo("Job Name", System.getenv("JOB_NAME"));
 		return extent;
 	 
 	}
